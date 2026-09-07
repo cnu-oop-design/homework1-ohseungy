05f68039e2fa0d58a9ee2449d59141cdcc19b2f8
User <oossyy08170@naver.com>
2026-09-07 10:47:02 +0900

    202302579 오승영

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-// 학번:
-// 이름:
+// 학번:202302579  
+// 이름:오승영
 
 int main(int argc, const char *argv[]) {
     // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
     //
     //   Hello, OOP!
     //   C++ is ready.
+    std::cout << "Hello, OOP!\n";
+    std::cout << "C++ is ready.\n";
 
     return 0;
 }
